9342e61b8770f6c1f693c684e72745815736af42
UraBoginskii <12345@gmail.com>
2022-12-23 22:58:09 +0300

    ДЗ семинар 3

---
+++ HomeWork_S3/Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿
+/*Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да*/
+
+
+Console.WriteLine("Введите пятизначное число");
+
+string str = Console.ReadLine();
+
+int[] b = new int[str.Length];
+
+for( int i=0; i < str.Length; i++)
+{
+     b[i] = int.Parse(str[i].ToString());
+}
+
+if ((b[0]==b[4]) & (b[1]==b[3])) Console.WriteLine($"Число {str} - палиндромом ");
+
+else Console.WriteLine($"Число {str} - не палиндромом ");
+
+
+
+
