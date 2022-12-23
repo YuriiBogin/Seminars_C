a2f5bb85a13b9f5d0a52393c0b46c1ad531e42ea
UraBoginskii <12345@gmail.com>
2022-12-23 20:58:58 +0300

    ДЗ семинар 2

---
+++ HomeWork_S2/Task_2.1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите  число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b =a;
+
+a = Math.Abs(a);
+
+if  (a>99) 
+{
+while (a >= 1000) a /= 10;
+
+int d = a % 10;
+
+Console.WriteLine($"Третья цифра числа {b} = {d} ");
+}
+else Console.WriteLine($"Третьей цифры в числе {b} - нет ");
